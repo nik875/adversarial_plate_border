69a90d13f36545413ad4620ccc78f507ddc0ecac
Nikhil Kalidasu <skdc13129@gmail.com>
2026-01-21 11:49:43 -0600

    Revert "Replace disruption mode OCR loss with smooth quadratic formula"
    This reverts commit f18ae506a38dffaca43a06b8de06f8f4cfd8dd19.

--- refine_generator.py
+++ refine_generator.py
@@ -663,9 +663,7 @@ class RefineGeneratorTrainer:
                 if self.impersonation_target:
                     ocr_loss = ocr_loss / self.ocr_baseline
                 else:
-                    # Disruption: maximize OCR loss with linear penalty
-                    # Neutral on variance; lets diversity emerge from other constraints (SSIM, TV)
-                    ocr_loss = 2 * (1 - ocr_loss)
+                    ocr_loss = self.ocr_baseline / ocr_loss
             else:
                 # For baseline calculation, return raw focal loss
                 pass
