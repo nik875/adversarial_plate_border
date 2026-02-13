6a8db70b04965e7a2b7c893653212d91c2a28251
Nikhil Kalidasu <skdc13129@gmail.com>
2026-02-13 10:43:09 -0600

    Use alphabet_36.txt for CRNN model

--- optimize_patch_cmaes.py
+++ optimize_patch_cmaes.py
@@ -386,7 +386,7 @@ def create_ocr_model(ocr_model_type, white_box=False):
             from onnx2pytorch import ConvertModel
 
             crnn_model_path = Path("text_recognition_CRNN_EN_2023feb_fp16.onnx")
-            crnn_dict_path = Path("alphabet_en.txt")
+            crnn_dict_path = Path("alphabet_36.txt")
 
             if not crnn_model_path.exists():
                 raise FileNotFoundError(
